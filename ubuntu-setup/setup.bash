1a5107dfb9d0cd9b9c51e7ac0f8b18b9b697b6b3
Shapedsundew9 <16618209+Shapedsundew9@users.noreply.github.com>
2023-07-01 15:48:05 -0400

    Update setup.bash

--- ubuntu-setup/setup.bash
+++ ubuntu-setup/setup.bash
@@ -93,7 +93,7 @@ echo "Cloning git repos..."
 mkdir -p ~/Projects
 cd ~/Projects
 
-REPOS="surebrec egp-population egp-seed experiments pypgtable egp-physics egp-gp-monitor egp-types toolbox egp-execution private_scripts obscure-password egp-utils egp-stores egp-worker egp-containers egp-execution"
+REPOS="egp-playground surebrec egp-population egp-seed experiments pypgtable egp-physics egp-gp-monitor egp-types toolbox egp-execution private_scripts obscure-password egp-utils egp-stores egp-worker egp-containers egp-execution"
 rm -f ~/.bash-ss
 touch ~/.bash-ss
 echo "export PYTHONPATH=." >> ~/.bashrc
